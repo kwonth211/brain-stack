158734777e07b8128f0bc048129104ebdd6bcb4b
truman <47585977+kwonth211@users.noreply.github.com>
2023-11-07 18:16:31 +0900

    fix: name 바꿀수 없도록

--- src/routes/api/user/[email]/+server.ts
+++ src/routes/api/user/[email]/+server.ts
@@ -1,37 +1,43 @@
-import { json } from '@sveltejs/kit';
-import { sql } from '@vercel/postgres';
-import type { User } from '../../../../types/user';
+import { json } from "@sveltejs/kit";
+import { sql } from "@vercel/postgres";
+import type { User } from "../../../../types/user";
 
-export async function PATCH({ request, params }: { request: Request; params: { email: string } }) {
-	const { email } = params;
-	const { name, nickname } = (await request.json()) as Partial<User>;
+export async function PATCH({
+  request,
+  params,
+}: {
+  request: Request;
+  params: { email: string };
+}) {
+  const { email } = params;
+  const { nickname } = (await request.json()) as Partial<User>;
 
-	const { rows: existingUsers } = await sql`SELECT * FROM users WHERE email=${email}`;
+  const { rows: existingUsers } =
+    await sql`SELECT * FROM users WHERE email=${email}`;
 
-	if (existingUsers.length === 0) {
-		return json({ error: '존재하지 않는 이메일 주소입니다.' }, { status: 404 });
-	}
+  if (existingUsers.length === 0) {
+    return json({ error: "존재하지 않는 이메일 주소입니다." }, { status: 404 });
+  }
 
-	let query = `UPDATE users SET `;
-	const paramsToUpdate: string[] = [];
-	const values: string[] = [];
+  let query = `UPDATE users SET `;
+  const paramsToUpdate: string[] = [];
+  const values: string[] = [];
 
-	if (name) {
-		paramsToUpdate.push(`name=$${paramsToUpdate.length + 1}`);
-		values.push(name);
-	}
-	if (nickname) {
-		paramsToUpdate.push(`nickname=$${paramsToUpdate.length + 1}`);
-		values.push(nickname);
-	}
+  if (nickname) {
+    paramsToUpdate.push(`nickname=$${paramsToUpdate.length + 1}`);
+    values.push(nickname);
+  }
 
-	query += paramsToUpdate.join(', ');
-	query += ` WHERE email=$${paramsToUpdate.length + 1}`;
-	values.push(email);
+  query += paramsToUpdate.join(", ");
+  query += ` WHERE email=$${paramsToUpdate.length + 1}`;
+  values.push(email);
 
-	if (paramsToUpdate.length > 0) {
-		await sql.query(query, values);
-	}
+  if (paramsToUpdate.length > 0) {
+    await sql.query(query, values);
+  }
 
-	return json({ message: '유저 정보가 성공적으로 업데이트되었습니다.' }, { status: 200 });
+  return json(
+    { message: "유저 정보가 성공적으로 업데이트되었습니다." },
+    { status: 200 }
+  );
 }
